612110c3bc8b0f8a59b9fd1568603f1a35a3fd38
Alexander Bjerre <alexander8@hotmail.com>
2020-12-18 13:53:53 +0100

    added compile time if and metaprogramming for battle functionality

--- I7APK/I7APK.cpp
+++ I7APK/I7APK.cpp
@@ -22,12 +22,16 @@ template<>
 struct TypeTraits<WeakType> {
 	static size_t hp() { return 50; };
 	static size_t attack() { return 10; };
+	static const int hp_c = 50;
+	static const int attack_c = 5;
 };
 
 template<>
 struct TypeTraits<StrongType> {
 	static size_t hp() { return 100; };
 	static size_t attack() { return 5; };
+	static const int hp_c = 100;
+	static const int attack_c = 13;
 };
 
 
@@ -61,6 +65,8 @@ class TypePokemon : public Pokemon {
 public:
 	TypePokemon(size_t index, std::string navn) : Pokemon(index, navn, TypeTraits<TType>::hp(), TypeTraits<TType>::attack())
 	{};
+
+	typedef TypeTraits<TType> type_traits;
 };
 
 using WeakPokemon = TypePokemon<WeakType>;
@@ -89,6 +95,35 @@ public:
 		//Look up in a compile time generated list?
 		return p1;
 	}
+};
+
+
+
+template<int health, int dmg>
+struct Attack {
+	inline static const int hp = (health - dmg) < 0 ? 0 : (health - dmg);
+};
+
+template<const int p1_dmg, const int p1_hp, const int p2_dmg, const int p2_hp>
+void Fight(const std::string& p1_name, const std::string& p2_name) {
+	std::cout << p1_name << " has " << p1_hp << " hp" << std::endl;
+
+	if constexpr (p1_hp == 0) {
+		std::cout << p1_name << " died for your entertainment" << std::endl;
+	}
+	else {
+		// give dmg
+		std::cout << p1_name << " survived and attacks " << p2_name << " for " << p1_dmg << " hp" << std::endl;
+		// next round
+		Fight<p2_dmg, Attack<p2_hp, p1_dmg>::hp, p1_dmg, p1_hp>(p2_name, p1_name);
+	}
+};
+
+template<typename P1, typename P2>
+void Fight(const P1& p1, const P2& p2) {
+	std::cout << "BATTLE MUSIC" << std::endl;
+
+	Fight<P1::type_traits::attack_c, P1::type_traits::hp_c, P2::type_traits::attack_c, P2::type_traits::hp_c>(p1._navn, p2._navn);
 };
 
 class Pokedex {
@@ -142,6 +177,13 @@ public:
 		boost::optional<Pokemon> winner = sig(_pokemons.front(), _pokemons.back());
 		std::cout << winner.get() << std::endl;
 	}
+
+	void predestinedBattle() {
+		WeakPokemon Raticate(20, "Raticate");
+		StrongPokemon Dragonite(149, "Dragonite");
+
+		Fight<WeakPokemon, StrongPokemon>(Raticate, Dragonite);
+	}
 };
 
 
@@ -158,6 +200,7 @@ int main()
 		std::cout << "Press 2 for viewing all pokemons with iterator" << std::endl;
 		std::cout << "Press 3 for viewing all pokemon names, sorted by name" << std::endl;
 		std::cout << "Press 4 for starting random fights every few seconds." << std::endl;
+		std::cout << "Press 5 for one predestined fight." << std::endl;
 		std::cout << "Press q for exiting" << std::endl;
 
 		std::cout << "Dit valg: ";
@@ -173,9 +216,11 @@ int main()
 			case '3':
 				pokedex.printSortedByName();
 				break;
-
 			case '4':
 				pokedex.randomPokemonFights();
+				break;
+			case '5':
+				pokedex.predestinedBattle();
 				break;
 			case 'q':
 				continueProgram = false;
